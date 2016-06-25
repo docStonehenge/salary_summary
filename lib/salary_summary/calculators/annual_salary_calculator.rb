6fe42d831bcdf91016944299e2229358953646e6
Victor Alexandre <victor.alexandrefs@gmail.com>
2016-06-25 14:59:33 -0300

    Adds salary sum algorithm to calculator

--- lib/salary_summary/calculators/annual_salary_calculator.rb
+++ lib/salary_summary/calculators/annual_salary_calculator.rb
@@ -1,16 +1,23 @@
 module SalarySummary
   module Calculators
     class AnnualSalaryCalculator
-      attr_reader :salaries
+      attr_reader   :salaries
+      attr_accessor :total_amount
 
       def initialize
-        @salaries = {}
+        @salaries, @total_amount = {}, 0
       end
 
       def enqueue(salary)
         salaries[
           salary.period.downcase.to_sym
         ] = salary.amount
+      end
+
+      def sum!
+        salaries.each do |_, amount|
+          self.total_amount += amount
+        end
       end
     end
   end
--- spec/salary_summary/calculators/annual_salary_calculator_spec.rb
+++ spec/salary_summary/calculators/annual_salary_calculator_spec.rb
@@ -3,15 +3,27 @@ require 'spec_helper'
 module SalarySummary
   module Calculators
     describe AnnualSalaryCalculator do
-      let(:salary) { double(:salary, period: 'January', amount: 1000.0) }
+      let(:january) { double(:salary, period: 'January', amount: 1000.0) }
 
-      it { is_expected.to have_attributes salaries: {} }
+      it { is_expected.to have_attributes salaries: {}, total_amount: 0 }
 
       describe '#enqueue salary' do
         it 'appends a salary to its hash with period as key' do
-          subject.enqueue salary
+          subject.enqueue january
 
           expect(subject.salaries).to include january: 1000.0
+        end
+      end
+
+      describe '#sum!' do
+        it 'sums up all salaries amounts' do
+          allow(subject).to receive(:salaries).and_return(
+                              january: 1000.0, february: 1550.0
+                            )
+
+          subject.sum!
+
+          expect(subject.total_amount).to eql 2550.0
         end
       end
     end
