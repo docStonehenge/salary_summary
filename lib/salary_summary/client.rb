c566d049878dc9e3882daed829513fdbaaabd848
Victor Alexandre <victor.alexandrefs@gmail.com>
2016-07-03 01:38:15 -0300

    Retrieves the database correctly from mongodb client

--- lib/salary_summary/client.rb
+++ lib/salary_summary/client.rb
@@ -1,7 +1,9 @@
 module SalarySummary
   class Client
     def self.database
-      @db ||= ::Mongo::Client.new('mongodb://127.0.0.1:27017/salary_summary')
+      @db ||= ::Mongo::Client.new(
+        'mongodb://127.0.0.1:27017/salary_summary'
+      ).database
     end
   end
 end
--- spec/salary_summary/client_spec.rb
+++ spec/salary_summary/client_spec.rb
@@ -3,13 +3,16 @@ require 'spec_helper'
 module SalarySummary
   describe Client do
     let(:mongodb_client) { double(:client) }
+    let(:database)       { double(:database) }
 
     it 'creates a MongoDB database and returns the client to be used' do
       allow(::Mongo::Client).to receive(:new).with(
                                 'mongodb://127.0.0.1:27017/salary_summary'
-                              ).and_return mongodb_client
+                                ).and_return mongodb_client
 
-      expect(described_class.database).to eql mongodb_client
+      expect(mongodb_client).to receive(:database).and_return database
+
+      expect(described_class.database).to eql database
     end
   end
 end
