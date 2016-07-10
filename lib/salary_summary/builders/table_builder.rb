55ff5d7a067ba7c91213f5e5bd35a6ac3c5d02e3
Victor Alexandre <victor.alexandrefs@gmail.com>
2016-07-10 01:09:11 -0300

    Builds using repository and calculator

--- lib/salary_summary/builders/table_builder.rb
+++ lib/salary_summary/builders/table_builder.rb
@@ -1,19 +1,15 @@
 module SalarySummary
   module Builders
     class TableBuilder
-      def initialize(using_repository:)
-        @repository = using_repository
-      end
-
-      def build_entries_on(collection_name)
-        @repository.find_on(collection_name).each do |entry|
+      def self.build_entries_on(collection_name)
+        Exporters::SalariesRepository.find_on(collection_name).each do |entry|
           puts "#{entry.dig('period')}----#{entry.dig('amount')}"
         end
       end
 
-      # def build_sum_footer
-      #   puts "Annual Salary----#{@container.total_amount}"
-      # end
+      def self.build_sum_footer_with(calculator)
+        puts "Annual Salary----#{calculator.sum}"
+      end
     end
   end
 end
--- spec/salary_summary/builders/table_builder_spec.rb
+++ spec/salary_summary/builders/table_builder_spec.rb
@@ -3,13 +3,13 @@ require 'spec_helper'
 module SalarySummary
   module Builders
     describe TableBuilder do
-      subject { described_class.new(using_repository: Exporters::SalariesRepository) }
+      let(:calculator) { double(:calculator) }
 
       before do
         $stdout = File.open(File::NULL)
       end
 
-      describe '#build_entries collection_name' do
+      describe '.build_entries collection_name' do
         it 'prints each entry on the table' do
           expect(
             Exporters::SalariesRepository
@@ -19,24 +19,24 @@ module SalarySummary
                ]
 
           expect {
-            subject.build_entries_on('salaries')
+            described_class.build_entries_on('salaries')
           }.to output(
                  "January----200.0\nFebruary----250.0\n"
                ).to_stdout
         end
       end
 
-      # describe '#build_sum_footer' do
-      #   it 'prints sum information footer on table' do
-      #     allow(salary_container).to receive(:total_amount).and_return 450.0
+      describe '.build_sum_footer_with calculator' do
+        it 'prints sum information footer on table' do
+          expect(calculator).to receive(:sum).and_return 450.0
 
-      #     expect {
-      #       subject.build_sum_footer
-      #     }.to output(
-      #            "Annual Salary----450.0\n"
-      #          ).to_stdout
-      #   end
-      # end
+          expect {
+            described_class.build_sum_footer_with(calculator)
+          }.to output(
+                 "Annual Salary----450.0\n"
+               ).to_stdout
+        end
+      end
     end
   end
 end
