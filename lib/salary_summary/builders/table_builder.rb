923a17050169c365a1f380afb0d92af67e523ddd
Victor Alexandre <victor.alexandrefs@gmail.com>
2016-08-06 17:50:39 -0300

    Builds table with salary correct information

--- lib/salary_summary/builders/table_builder.rb
+++ lib/salary_summary/builders/table_builder.rb
@@ -5,14 +5,14 @@ module SalarySummary
         @repository = repository
       end
 
-      def build_entries_on(collection_name)
-        @repository.find_on(collection_name).each do |salary|
-          puts "#{salary.period}----#{salary.amount}"
+      def build_entries
+        @repository.find.each do |salary|
+          puts "#{salary.month}, #{salary.year} ---- #{salary.amount}"
         end
       end
 
-      def build_sum_footer_for(collection_name)
-        puts "Annual Salary----#{@repository.sum(collection_name)}"
+      def build_sum_footer
+        puts "Total ---- #{@repository.sum}"
       end
     end
   end
--- spec/salary_summary/builders/table_builder_spec.rb
+++ spec/salary_summary/builders/table_builder_spec.rb
@@ -4,8 +4,8 @@ module SalarySummary
   module Builders
     describe TableBuilder do
       let(:repository) { double(:repository) }
-      let(:salary_1)   { double(:salary, period: 'January', amount: 200.0) }
-      let(:salary_2)   { double(:salary, period: 'February', amount: 250.0) }
+      let(:salary_1)   { double(:salary, month: 'January', year: 2016, amount: 200.0) }
+      let(:salary_2)   { double(:salary, month: 'February', year: 2016, amount: 250.0) }
 
       before do
         $stdout = File.open(File::NULL)
@@ -13,30 +13,25 @@ module SalarySummary
 
       subject { described_class.new(repository) }
 
-      describe '#build_entries collection_name' do
+      describe '#build_entries' do
         it 'prints each entry on the table' do
-          expect(
-            repository
-          ).to receive(:find_on).with('salaries').and_return [salary_1, salary_2]
-
+          expect(repository).to receive(:find).and_return [salary_1, salary_2]
 
           expect {
-            subject.build_entries_on('salaries')
+            subject.build_entries
           }.to output(
-                 "January----200.0\nFebruary----250.0\n"
+                 "January, 2016 ---- 200.0\nFebruary, 2016 ---- 250.0\n"
                ).to_stdout
         end
       end
 
-      describe '#build_sum_footer_for collection_name' do
+      describe '#build_sum_footer' do
         it 'prints sum information footer on table' do
-          expect(repository).to receive(:sum).with('salaries').and_return 450.0
+          expect(repository).to receive(:sum).and_return 450.0
 
           expect {
-            subject.build_sum_footer_for('salaries')
-          }.to output(
-                 "Annual Salary----450.0\n"
-               ).to_stdout
+            subject.build_sum_footer
+          }.to output("Total ---- 450.0\n").to_stdout
         end
       end
     end
