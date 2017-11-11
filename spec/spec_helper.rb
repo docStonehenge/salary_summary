ea2c23835a5ee7b186dd616e53629abeb9f8d095
Victor Alexandre <victor.alexandrefs@gmail.com>
2017-11-11 13:31:40 -0200

    Fixes SimpleCov configuration; sets stubbed test for exporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,11 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'salary_summary'
-
 require 'simplecov'
 
 SimpleCov.start do
   add_filter '/spec/'
 end
+
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'salary_summary'
 
 ENV['ENVIRONMENT'] = 'test'
 
