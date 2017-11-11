3857fb2443a731301c99241d91b55a674648671f
Victor Alexandre <victor.alexandrefs@gmail.com>
2017-11-11 13:19:44 -0200

    Installs and configures simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,12 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'salary_summary'
 
+require 'simplecov'
+
+SimpleCov.start do
+  add_filter '/spec/'
+end
+
 ENV['ENVIRONMENT'] = 'test'
 
 Dir["spec/support/**/*.rb"].each { |f| load f }
