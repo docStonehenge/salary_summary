8bebf186b095e3e17dbfe6175bd41d06d5fe347d
Victor Alexandre <victor.alexandrefs@gmail.com>
2017-11-21 21:52:11 -0200

    Sets configuration for clearing test database before specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,3 +10,14 @@ require 'salary_summary'
 ENV['ENVIRONMENT'] = 'test'
 
 Dir["spec/support/**/*.rb"].each { |f| load f }
+
+RSpec.configure do |config|
+  config.before(:each, type: :database_integration) do
+    client = ::Mongo::Client.new(
+      SalarySummary::Databases::URIParser.parse_based_on_file
+    )
+
+    client.database.collections.each(&:drop)
+    client.close
+  end
+end
